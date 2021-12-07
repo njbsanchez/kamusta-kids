ee672cce51cf2f9663045d4722760e344cfa5118
Nicole Sanchez <njbsanchez@gmail.com>
2021-12-06 22:44:45 -0800

    created database

--- internal/model.py
+++ internal/model.py
@@ -4,12 +4,12 @@ import datetime
 
 db = SQLAlchemy()
 
-def connect_to_db(flask_app, dbname='inventory_psql', echo=False):  
+def connect_to_db(flask_app, dbname='kamusta_psql', echo=False):  
     
     #organization info
-    flask_app.config['ORG_NAME'] = 'MJ'
-    flask_app.config['APP_TITLE'] = 'Sales & Inventory DB'
-    flask_app.config['APP_DESC'] = 'An extremely simple way to track the sales and inventory of cannabis products.'
+    flask_app.config['ORG_NAME'] = 'Kamusta Kids'
+    flask_app.config['APP_TITLE'] = 'Website & System'
+    flask_app.config['APP_DESC'] = 'Kamusta Kids external website and internal administrative system'
 
     #database
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql:///' + dbname
@@ -30,140 +30,140 @@ def connect_to_db(flask_app, dbname='inventory_psql', echo=False):
 # Helper Functions
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-def sku_get_intake_quantity(sku):
-    """quantity from intake"""
+                    # def sku_get_intake_quantity(sku):
+                    #     """quantity from intake"""
 
-    return Intake.query.filter(Intake.sku == sku).first()
+                    #     return Intake.query.filter(Intake.sku == sku).first()
 
-def sku_get_sale_quantity(sku):
-    """add quantity of all sales"""
-    
-    items_with_sku = Item.query.filter(Item.sku == sku).all()
-    return_data = {"quantity_total": 0, "sale_records":[]}
-    
-    for item in items_with_sku:
-        return_data["quantity_total"] += item.quantity
-        return_data["sale_records"].append(item)
-        
-    return return_data     
+                    # def sku_get_sale_quantity(sku):
+                    #     """add quantity of all sales"""
+                        
+                    #     items_with_sku = Item.query.filter(Item.sku == sku).all()
+                    #     return_data = {"quantity_total": 0, "sale_records":[]}
+                        
+                    #     for item in items_with_sku:
+                    #         return_data["quantity_total"] += item.quantity
+                    #         return_data["sale_records"].append(item)
+                            
+                    #     return return_data     
 
-def sku_get_sample_quantity(sku):   
-    """add quantity of all samples out"""
-    
-    sampleitems_with_sku = SampleItem.query.filter(SampleItem.sku == sku).all()
-    return_data = {"sampleout":{"quantity_total": 0, "sample_records":[]}, "returned": {"quantity_total": 0, "sample_records":[]}}
-    
-    for sampleitem in sampleitems_with_sku:
-        
-        sample_record = Sample.query.filter(Sample.id == sampleitem.sale_id).first()
-        if sample_record.movement == "sampleout":
-            return_data["sampleout"]["quantity_total"] += sampleitem.quantity
-            return_data["sampleout"]["sample_records"].append(sampleitem.id)
-        if sample_record.movement == "samplereturn":
-            return_data["returned"]["quantity_total"] += sampleitem.quantity
-            return_data["returned"]["sample_records"].append(sampleitem.id)
+                    # def sku_get_sample_quantity(sku):   
+                    #     """add quantity of all samples out"""
+                        
+                    #     sampleitems_with_sku = SampleItem.query.filter(SampleItem.sku == sku).all()
+                    #     return_data = {"sampleout":{"quantity_total": 0, "sample_records":[]}, "returned": {"quantity_total": 0, "sample_records":[]}}
+                        
+                    #     for sampleitem in sampleitems_with_sku:
+                            
+                    #         sample_record = Sample.query.filter(Sample.id == sampleitem.sale_id).first()
+                    #         if sample_record.movement == "sampleout":
+                    #             return_data["sampleout"]["quantity_total"] += sampleitem.quantity
+                    #             return_data["sampleout"]["sample_records"].append(sampleitem.id)
+                    #         if sample_record.movement == "samplereturn":
+                    #             return_data["returned"]["quantity_total"] += sampleitem.quantity
+                    #             return_data["returned"]["sample_records"].append(sampleitem.id)
 
-    return return_data  
+                    #     return return_data  
 
-def calculate_quantity_instock(sku):
-    """quantity left = intake minus sales, minus samples out, add sample returned"""
+                    # def calculate_quantity_instock(sku):
+                    #     """quantity left = intake minus sales, minus samples out, add sample returned"""
 
-    quant_intake = sku_get_intake_quantity(sku)
-    quant_sold = sku_get_sale_quantity(sku)
-    quant_dict = sku_get_sample_quantity(sku)
-    quant_sample_out = quant_dict["sampleout"]
-    quant_sample_returned = quant_dict["returned"]
-    
-    quant_instock = quant_intake - quant_sold["quantity_total"] - quant_sample_out["quantity_total"] + quant_sample_returned["quantity_total"]
-    
-    return quant_instock
+                    #     quant_intake = sku_get_intake_quantity(sku)
+                    #     quant_sold = sku_get_sale_quantity(sku)
+                    #     quant_dict = sku_get_sample_quantity(sku)
+                    #     quant_sample_out = quant_dict["sampleout"]
+                    #     quant_sample_returned = quant_dict["returned"]
+                        
+                    #     quant_instock = quant_intake - quant_sold["quantity_total"] - quant_sample_out["quantity_total"] + quant_sample_returned["quantity_total"]
+                        
+                    #     return quant_instock
 
-# By Product ~~~~~~~~~~~~~~
+                    # # By Product ~~~~~~~~~~~~~~
 
-def prod_get_intake_quantity(product_id):
-    """all intake with given product id"""
-    
-    intake_quant = 0
-    intake_with_id = Intake.query.filter(Intake.product_id == product_id).all()
-    
-    for intake in intake_with_id:
-            intake_quant += intake.quantity
-    
-    return intake_quant
-   
-def prod_get_sale_quantity(product_id):
-    """from sales - add quantity with given product id"""
+                    # def prod_get_intake_quantity(product_id):
+                    #     """all intake with given product id"""
+                        
+                    #     intake_quant = 0
+                    #     intake_with_id = Intake.query.filter(Intake.product_id == product_id).all()
+                        
+                    #     for intake in intake_with_id:
+                    #             intake_quant += intake.quantity
+                        
+                    #     return intake_quant
+                    
+                    # def prod_get_sale_quantity(product_id):
+                    #     """from sales - add quantity with given product id"""
 
-    sale_quant = 0
-    items_with_id = Item.query.filter(Item.product_id == product_id).all()
-    
-    for item in items_with_id:
-            sale_quant += item.quantity
-    
-    return sale_quant
-    
-def prod_get_sample_quantity(product_id): 
-    """from samples - add quantity with given product id"""
+                    #     sale_quant = 0
+                    #     items_with_id = Item.query.filter(Item.product_id == product_id).all()
+                        
+                    #     for item in items_with_id:
+                    #             sale_quant += item.quantity
+                        
+                    #     return sale_quant
+                        
+                    # def prod_get_sample_quantity(product_id): 
+                    #     """from samples - add quantity with given product id"""
 
-    sampleitems_with_id = SampleItem.query.filter(SampleItem.product_id == product_id).all()
-    return_data = {"sampleout":{"quantity_total": 0, "sample_records":[]}, "returned": {"quantity_total": 0, "sample_records":[]}}
-    
-    for sampleitem in sampleitems_with_id:
-        
-        sample_record = Sample.query.filter(Sale.id == sampleitem.sale_id).first()
-        if sample_record.movement == "sampleout":
-            return_data["sampleout"]["quantity_total"] += sampleitem.quantity
-            return_data["sampleout"]["sample_records"].append(sampleitem.sample_record_id)
-        if sample_record.movement == "samplereturn":
-            return_data["returned"]["quantity_total"] += sampleitem.quantity
-            return_data["returned"]["sample_records"].append(sampleitem.sample_record_id)
+                    #     sampleitems_with_id = SampleItem.query.filter(SampleItem.product_id == product_id).all()
+                    #     return_data = {"sampleout":{"quantity_total": 0, "sample_records":[]}, "returned": {"quantity_total": 0, "sample_records":[]}}
+                        
+                    #     for sampleitem in sampleitems_with_id:
+                            
+                    #         sample_record = Sample.query.filter(Sale.id == sampleitem.sale_id).first()
+                    #         if sample_record.movement == "sampleout":
+                    #             return_data["sampleout"]["quantity_total"] += sampleitem.quantity
+                    #             return_data["sampleout"]["sample_records"].append(sampleitem.sample_record_id)
+                    #         if sample_record.movement == "samplereturn":
+                    #             return_data["returned"]["quantity_total"] += sampleitem.quantity
+                    #             return_data["returned"]["sample_records"].append(sampleitem.sample_record_id)
 
-    return return_data  
+                    #     return return_data  
 
-def prod_calculate_quantity_instock(product_id):
-    """quantity left = intake minus sales, minus samples out, add sample returned"""
+                    # def prod_calculate_quantity_instock(product_id):
+                    #     """quantity left = intake minus sales, minus samples out, add sample returned"""
 
-    quant_intake = prod_get_intake_quantity(product_id)
-    quant_sold = prod_get_sale_quantity(product_id)
-    quant_dict = prod_get_sample_quantity(product_id)
-    quant_sample_out = quant_dict["sampleout"]
-    quant_sample_returned = quant_dict["returned"]
-    
-    quant_instock = quant_intake - quant_sold["quantity_total"] - quant_sample_out["quantity_total"] + quant_sample_returned["quantity_total"]
-    
-    return quant_instock
+                    #     quant_intake = prod_get_intake_quantity(product_id)
+                    #     quant_sold = prod_get_sale_quantity(product_id)
+                    #     quant_dict = prod_get_sample_quantity(product_id)
+                    #     quant_sample_out = quant_dict["sampleout"]
+                    #     quant_sample_returned = quant_dict["returned"]
+                        
+                    #     quant_instock = quant_intake - quant_sold["quantity_total"] - quant_sample_out["quantity_total"] + quant_sample_returned["quantity_total"]
+                        
+                    #     return quant_instock
 
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~           
-# Money Calculations
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    # # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~           
+                    # # Money Calculations
+                    # # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Item Instance Calc ~~~~~~~~~~~~~~
+                    # # Item Instance Calc ~~~~~~~~~~~~~~
 
-def calc_sub_total(quantity, item_instance):
-    
-    prem_disc = item_instance.sale.prem_disc_percentage
-    prem_disc = (100 + prem_disc) / 100
-    new_price = item_instance.intake_instance.selling_price *  prem_disc
-    
-    print("premium or discount: ", prem_disc)
-    print("quantity: ", quantity)
-    print("new price per item: ", new_price)
-    
-    sub_total = quantity * new_price
+                    # def calc_sub_total(quantity, item_instance):
+                        
+                    #     prem_disc = item_instance.sale.prem_disc_percentage
+                    #     prem_disc = (100 + prem_disc) / 100
+                    #     new_price = item_instance.intake_instance.selling_price *  prem_disc
+                        
+                    #     print("premium or discount: ", prem_disc)
+                    #     print("quantity: ", quantity)
+                    #     print("new price per item: ", new_price)
+                        
+                    #     sub_total = quantity * new_price
 
-    print("subtotal: ", sub_total)
+                    #     print("subtotal: ", sub_total)
 
-    return sub_total
+                    #     return sub_total
 
-def calc_cogs(quantity, item_instance):
-    
-    cost_per_unit = item_instance.intake_instance.cost_per_unit
-    licensing_per_unit = item_instance.intake_instance.licensing_fee
-    
-    cogs_of_item = quantity * (cost_per_unit + licensing_per_unit)
-    
-    return cogs_of_item
+                    # def calc_cogs(quantity, item_instance):
+                        
+                    #     cost_per_unit = item_instance.intake_instance.cost_per_unit
+                    #     licensing_per_unit = item_instance.intake_instance.licensing_fee
+                        
+                    #     cogs_of_item = quantity * (cost_per_unit + licensing_per_unit)
+                        
+                    #     return cogs_of_item
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 
-# from internal.model import db, connect_to_db
+from internal.model import db, connect_to_db
 from app import app
 # import internal.routes
 import external.routes
 
 if __name__ == '__main__':
     
-    # connect_to_db(app)
-    # db.create_all()
+    connect_to_db(app)
+    db.create_all()
     app.run()
         

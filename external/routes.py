98906b3d7dd54153c6ccd43fdfd95bbe2587c0df
Nicole Sanchez <njbsanchez@gmail.com>
2022-01-08 18:25:46 -0800

    links added to all buttons

--- external/routes.py
+++ external/routes.py
@@ -5,39 +5,40 @@ import time
 import datetime
 from model import Category, Course
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def go_homepage():
     """dummy development page"""
-    return render_template("homepage.html")
+    course_detail = Course.query.first()
+    return render_template("homepage.html", course_detail=course_detail)
 
-@app.route("/about")
+@app.route("/about", methods=['GET'])
 def go_about():
     """dummy development page"""
     return render_template("aboutus.html")
 
-@app.route("/how-learning-works")
+@app.route("/how-learning-works", methods=['GET'])
 def go_learning():
     """dummy development page"""
     return render_template("learningworks.html")
 
-@app.route("/courses")
+@app.route("/courses", methods=['GET'])
 def go_courses():
     """dummy development page"""
     cat_details = Category.query.order_by(Category.cat_no).all()
     
     return render_template("courses.html", cat_details=cat_details)
 
-@app.route("/request-info")
+@app.route("/request-info", methods=['GET'])
 def request_info():
     """dummy development page"""
     return render_template("request_info.html")
 
-@app.route("/go-enroll")
+@app.route("/go-enroll", methods=['GET'])
 def go_enroll():
     """dummy development page"""
     return render_template("coming_soon.html")
 
-@app.route("/coming-soon")
+@app.route("/coming-soon", methods=['GET'])
 def coming_soon():
     """dummy development page"""
     return render_template("coming_soon.html")
